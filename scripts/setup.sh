92a0faadf6664f79ec8cb333de7d2921154b6302
Seifeldin Sabry <93074813+Seifeldin-Sabry@users.noreply.github.com>
2023-07-08 01:07:57 +0300

    finished setup script

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -35,12 +35,18 @@ function help(){
 }
 
 function copy_dot_files() {
+  echo "=====================
+  Copying dotfiles...
+  ====================="
   ln -s ~/.dotfiles/.bash_profile ~/.bash_profile
   ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
   ln -s ~/.dotfiles/.iterm2_shell_integration.zsh ~/.iterm2_shell_integration.zsh
 }
 
 function zsh() {
+  echo "=====================
+  Installing zsh...
+  ====================="
   sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
   mv ~/.zshrc ~/.zshrc.bak
   ln -s ~/.dotfiles/.zshrc ~/.zshrc
@@ -54,27 +60,71 @@ function check_help() {
   fi
 }
 
+function copy_ssh_keys() {
+  echo "=====================
+  Copying ssh keys...
+  ====================="
+  mkdir ~/.ssh
+  IFS='
+  '
+  for file in $(ls -a ~/.dotfiles/directories/.ssh); do
+    if [ "$file" != "." ] && [ "$file" != ".." ]
+    then
+      cp ~/.dotfiles/directories/.ssh/"$file" ~/.ssh/"$file"
+      ansible-vault decrypt ~/.ssh/"$file"
+      chmod 700 ~/.ssh/"$file"
+      ssh-add -K ~/.ssh/"$file"
+    fi
+  done
+  eval "$(ssh-agent -s)"
 
-OS=$1
+  touch ~/.ssh/config
+  echo -e "Host *\n AddKeysToAgent yes\n UseKeychain yes\n IdentityFile ~/.ssh/id_ed25519" > ~/.ssh/config
 
-# check if parameter is passed
-if [ -z "$OS" ]; then
-  echo "No OS specified. Exiting."
-  help
-  exit 1
-elif [ "$OS" != "macos" ] && [ "$OS" != "linux" ]; then
-    echo "OS not supported. Exiting."
+  unset IFS
+}
+
+function check_os(){
+  OS=$1
+  # check if parameter is passed
+  if [ -z "$OS" ]; then
+    echo "No OS specified. Exiting."
     help
     exit 1
-fi
+  elif [ "$OS" != "macos" ] && [ "$OS" != "linux" ]; then
+      echo "OS not supported. Exiting."
+      help
+      exit 1
+  fi
+}
+
+function xcode() {
+  echo "=====================
+  Installing xcode...
+  ====================="
+  if [ "$OS" == "macos" ]
+  then
+    xcode-select --install
+  else
+    echo "Skipping xcode install for linux...."
+  fi
+}
 
 check_help "$1"
 check_root
-if [ "$OS" == "macos" ]; then
-  xcode-select --install
-fi
+check_os "$1"
+
+echo "=====================
+Installing dotfiles for $OS...
+====================="
+
+xcode
 copy_dot_files
 brew
+copy_ssh_keys
 zsh
 
+echo -e "=====================
+For Raycast, open raycast and type \"Import Settings & Data\" and select the file \"raycast.config\" in the dotfiles directory
+====================="
 
