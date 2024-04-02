5a3f16b6fc3a748efe883cc5ce74109e1aa0950f
Seifeldin Sabry <93074813+Seifeldin-Sabry@users.noreply.github.com>
2024-04-02 10:05:02 +0200

    Update Brewfile, nvm

--- files/.zshrc
+++ files/.zshrc
@@ -149,3 +149,6 @@ alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 
 export NVM_DIR="$HOME/.nvm"
 [ -s "$HOMEBREW_PREFIX/opt/nvm/nvm.sh" ] && . "$HOMEBREW_PREFIX/opt/nvm/nvm.sh"
+export PYENV_ROOT="$HOME/.pyenv"
+command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"
+eval "$(pyenv init -)"
--- update.sh
+++ update.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
-BREWFILE_LOCATION=~/.dotfiles/Brewfile
-brew bundle dump --file=$BREWFILE_LOCATION --force
+USER_NOW=$(whoami)
+DIR="/Users/$USER_NOW/.dotfiles"
+# format date example: Mon 01 Jan 2021 12:00:00 PM
+DATE=$(date +"%a %d %b %Y %I:%M:%S %p")
+echo "Updating dotfiles on $DATE"
 
-NVM_DIR=~/.dotfiles/directories/.nvm/
+LOGFILE="$DIR/logs/$DATE.log"
 
-git add $BREWFILE_LOCATION $NVM_DIR
-git commit -m "Update Brewfile, nvm"
-git push origin main
+# make sure the log file exists if not create it
+echo "Creating log file at $LOGFILE"
+
+if [ ! -f "$LOGFILE" ]; then
+  touch "$LOGFILE"
+fi
+
+BREWFILE_LOCATION=$DIR/.dotfiles/Brewfile
+brew bundle dump --file=$BREWFILE_LOCATION --force 2> "$LOGFILE"
+
+NVM_DIR=$DIR/.dotfiles/directories/.nvm/ 2> "$LOGFILE"
+cd $DIR
+git pull origin main 2> "$LOGFILE"
+git add . 2> "$LOGFILE"
+git commit -m "Update Brewfile, nvm" 2> "$LOGFILE"
+git push origin main 2> "$LOGFILE"
+cd -
